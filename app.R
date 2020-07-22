a1e1ad3fa7bbafea24c3d8d890f8a380501643e9
Rayland Matos Magalhães <39091838+raylandmagalhaes@users.noreply.github.com>
2020-07-22 12:05:22 -0300

    forgot to comit

--- app.R
+++ app.R
@@ -4,8 +4,7 @@ library(plotly)
 library(shinydashboard)
 library(DT)
 
-camara=bind_rows(list(camara2009,camara2010,camara2011,camara2012,camara2013,camara2014,camara2015,camara2016,camara2017,camara2018,camara2019)) %>% filter(total_net_value>0) %>% data.frame(check.names = F) %>% 
-  filter(!is.na(total_net_value)&!is.na(document_value)) 
+load("camara")
 
 
 shinyApp(
